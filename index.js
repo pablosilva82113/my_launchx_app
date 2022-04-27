20214b50f81ceaceb8ee4bf2dbe9af5a4089ee07
pablosilva82113 <pablosilva82113@gmail.com>
2022-04-27 10:56:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Pablo")
+console.log(carlo.name)
+
+const pablo = new MissionCommander("Pablo")
+console.log(pablo.name)
+
+const juan = new MissionCommander("Juan")
+console.log(juan.name)
---
+++ test/missionCommander.test.js
@@ -0,0 +1,14 @@
+
+describe("Esto es una suite de pruebas", () => {
+    test('Caso de prueba 1', () => {
+      const result = 1 + 2 
+      expect(result).toBe(10);
+    });
+  })
+  
+  describe("Unit Tests for Mission Commander Class", () => {
+    test('1) Create a mission commander objet', () => {
+        const myMissionCommander = new MissionCommander("Woopa")
+        expect(myMissionCommander.name).toBe("Woopa")
+    });
+})
